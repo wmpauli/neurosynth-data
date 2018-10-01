1ae305be039174f0bbd12b99af46ced7a4a4068a
Wolfgang Pauli <Wolfgang.m.Pauli@gmail.com>
2018-10-01 00:45:06 -0700

    script for downloading data from osf

---
+++ download_feature_images.sh
@@ -0,0 +1,7 @@
+# run this script for downloading the feature images, which would take
+# forever to create.  Note, these files are large 2-3GB, so even
+# downloading them takes a while
+
+wget https://osf.io/9v3d6/download -O feature_images_human.npy
+wget https://osf.io/9m6cu/download -O feature_images_rat.npy
+wget https://osf.io/yv8c2/download -O feature_names.txt
